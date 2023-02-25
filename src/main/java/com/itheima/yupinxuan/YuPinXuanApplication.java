f73f154aef0665852b1f5572dda8c02b8325c140
preyusity <82532705+preyusity@users.noreply.github.com>
2023-02-25 15:47:13 +0800

    配置pom文件，配置yml文件，添加前端资源

--- src/main/java/com/itheima/yupinxuan/YuPinXuanApplication.java
+++ src/main/java/com/itheima/yupinxuan/YuPinXuanApplication.java
@@ -1,7 +1,14 @@
 package com.itheima.yupinxuan;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
 public class YuPinXuanApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(YuPinXuanApplication.class,args);
+        log.info("项目启动成功....");
     }
 }
---
+++ src/main/java/com/itheima/yupinxuan/config/webMvcConfig.java
@@ -0,0 +1,16 @@
+package com.itheima.yupinxuan.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class webMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
