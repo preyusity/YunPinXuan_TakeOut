9b85002070e4fb0faecba154e90ca7b2298e15d3
preyusity <82532705+preyusity@users.noreply.github.com>
2023-02-25 18:02:51 +0800

    登录接口模版

---
+++ src/main/java/com/itheima/yupinxuan/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.itheima.yupinxuan.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.yupinxuan.Employee.Employee;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmployeeService extends IService<Employee> {
+}
