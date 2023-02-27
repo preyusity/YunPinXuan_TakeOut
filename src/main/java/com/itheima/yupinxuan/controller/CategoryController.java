598703e3f564df3a0a1ac8fb376ab8405fa61ad7
litianjian <LItianjian1234>
2023-02-28 03:29:17 +0800

    完成分类管理所有接口开发

--- src/main/java/com/itheima/yupinxuan/controller/CategoryController.java
+++ src/main/java/com/itheima/yupinxuan/controller/CategoryController.java
@@ -55,4 +55,16 @@ public class CategoryController {
         categoryService.remove(id);
         return R.success("删除成功");
     }
+
+    /**修改分类信息
+     *
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+
+    }
 }
